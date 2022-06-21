cb33f016da40b11ed6b77b5c81716e6aee269e2a
Caitlyn <caitlyn@oppkey.com>
2022-06-21 07:45:38 -0700

    added chopper files

--- lib/blocs/camera_use/camera_use_bloc.dart
+++ lib/blocs/camera_use/camera_use_bloc.dart
@@ -1,11 +1,14 @@
 import 'package:bloc/bloc.dart';
+import 'package:chopper/chopper.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
 
 part 'camera_use_event.dart';
 part 'camera_use_state.dart';
 
 class CameraUseBloc extends Bloc<CameraUseEvent, CameraUseState> {
-  CameraUseBloc() : super(CameraUseInitial()) {
+  CameraUseBloc() : super(CameraUseState.initial()) {
+    var chopper = ChopperClient();
     on<CameraUseEvent>((event, emit) {
       // TODO: implement event handler
     });
--- lib/blocs/camera_use/camera_use_state.dart
+++ lib/blocs/camera_use/camera_use_state.dart
@@ -1,10 +1,24 @@
 part of 'camera_use_bloc.dart';
 
-abstract class CameraUseState extends Equatable {
-  const CameraUseState();
-  
-  @override
-  List<Object> get props => [];
-}
+class CameraUseState extends Equatable {
+  final String responseMessage;
 
-class CameraUseInitial extends CameraUseState {}
+  const CameraUseState({required this.responseMessage});
+
+  factory CameraUseState.initial() =>
+      CameraUseState(responseMessage: 'waiting for response');
+
+  @override
+  List<Object> get props => [responseMessage];
+
+  @override
+  bool get stringify => true;
+
+  CameraUseState copyWith({
+    String? responseMessage,
+  }) {
+    return CameraUseState(
+      responseMessage: responseMessage ?? this.responseMessage,
+    );
+  }
+}
